aff5aec7e50bb071547d869e7a016678b5aa30f5
ouanoughi <ouanoughiamine1995@gmail.com>
2024-03-23 18:34:41 +0100

    deploy: project

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,8 +3,8 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className='footer'>
-            <a href="https://github.com/ouanoughi-amine"><img src="./src/assets/github.svg" alt="github" className='photo'/></a>
-            <a href="https://www.linkedin.com/in/amine-ouanoughi"><img src="./src/assets/linkedin.svg" alt="linkedin" className='photo'/></a>
+            <a href="https://github.com/ouanoughi-amine"><img src="./github.svg" alt="github" className='photo'/></a>
+            <a href="https://www.linkedin.com/in/amine-ouanoughi"><img src="./linkedin.svg" alt="linkedin" className='photo'/></a>
 
         </div>
     );
--- src/Components/SectionFiles/SectionCompetences.jsx
+++ src/Components/SectionFiles/SectionCompetences.jsx
@@ -8,24 +8,24 @@ const SectionCompetences = () => {
    <div className="competences">
         <div className='competences__card'>
             <h2 className='title'>Front-end</h2>
-            <img src="./src/assets/html.svg" alt="logo html" className="image"/>
-            <img src="./src/assets/css.svg" alt="logo css" className="image"/>
-            <img src="./src/assets/js.svg" alt="logo javascript" className="image"/>
-            <img src="./src/assets/react.svg" alt="logo react" className="image"/>
+            <img src="./html.svg" alt="logo html" className="image"/>
+            <img src="./css.svg" alt="logo css" className="image"/>
+            <img src="./js.svg" alt="logo javascript" className="image"/>
+            <img src="./react.svg" alt="logo react" className="image"/>
         </div>
         <div className='competences__card'>
             <h2 className='title'>Back-end</h2>
-            <img src="./src/assets/node.svg" alt="logo node" className="image"/>
-            <img src="./src/assets/express.svg" alt="logo express" className="image"/>
+            <img src="./node.svg" alt="logo node" className="image"/>
+            <img src="./express.svg" alt="logo express" className="image"/>
             <h2 className='title'>Base de données</h2>
-            <img src="./src/assets/mongo.svg" alt="logo mongo" className="image"/>
+            <img src="./mongo.svg" alt="logo mongo" className="image"/>
         </div>
         <div className='competences__card'>
             <h2 className='title'>Outils</h2>
-            <img src="./src/assets/figma.svg" alt="logo figma" className="image"/>
-            <img src="./src/assets/trello.svg" alt="logo trello" className="image"/>
-            <img src="./src/assets/github.svg" alt="logo github" className="image"/>
-            <img src="./src/assets/vscode.svg" alt="logo vscode" className="image"/>
+            <img src="./figma.svg" alt="logo figma" className="image"/>
+            <img src="./trello.svg" alt="logo trello" className="image"/>
+            <img src="./github.svg" alt="logo github" className="image"/>
+            <img src="./vscode.svg" alt="logo vscode" className="image"/>
         </div>
     </div>
 </div>
